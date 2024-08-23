e9038673bce1919e99942696a910fb50da3a62cd
Roman <dlcource_rccf@gmail.com>
2024-08-23 19:02:00 +0300

    switched to hydra

--- src/model_1/download/download.py
+++ src/model_1/download/download.py
@@ -1,10 +1,11 @@
 import papermill as pm
 
+
 def download():
     pm.execute_notebook(
     input_path='src/model_1/download/download.ipynb',
     output_path='src/model_1/download/download_out.ipynb',
-    kernel_name='dvc-template-4'
+    kernel_name='dvc-template-7'
 )
 
 if __name__ == '__main__':
